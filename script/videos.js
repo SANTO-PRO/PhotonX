3da52bcb5cf6da2313e1ada895a01caff36fb4c2
Saidul Islam Santo <sksanto.pro@gmail.com>
2022-12-14 13:31:36 +0600

    video Js

--- script/videos.js
+++ script/videos.js
@@ -1,15 +1,37 @@
+//==> Selectors:
+//=> Divs:
 const videosGallery = document.querySelector('.videos-gallery');
+const searchInput = document.querySelector('.videosSearchInput');
+const form = document.querySelector('.videosSearchForm');
 
+//=> Btns:
 const moreVideosBtn = document.querySelector('.moreVideos-Btn');
 
-let fetchVideosUrl;
-
-//=> Load More Videos:
-moreVideosBtn.addEventListener('click', loadMoreVideos);
+//==> Variables:
+let searchValue;
+let currentSearch;
+let page = 1;
 
 //==> API Elements:
 //=> API KEY:
 const auth_key = '563492ad6f917000010000013457deb42ca2403a9074ac93b29156e6';
+
+//==> Listners:
+//=> Search Input:
+searchInput.addEventListener('input', updateInput);
+
+console.log(searchInput);
+
+//=> Form:
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	currentSearch = searchValue;
+	searchVideos(searchValue);
+});
+
+//=> Load More Videos:
+moreVideosBtn.addEventListener('click', loadMoreVideos);
 
 //==> Functions:
 //-->> Update Input:
@@ -19,7 +41,7 @@ function updateInput(e) {
 
 //-->> Clear:
 function clear() {
-	photosGallery.innerHTML = '';
+	videosGallery.innerHTML = '';
 	searchInput.value = '';
 }
 
@@ -43,18 +65,52 @@ async function fetchApi(url) {
 function generateVideos(data) {
 	data.videos.forEach((video) => {
 		video.video_files.forEach((video_file) => {
-			const photoContainer = document.createElement('div');
+			//=> Create Video Container:
+			const videoContainer = document.createElement('div');
+			videoContainer.classList.add('videoContainer');
 
-			photoContainer.classList.add('photoContainer');
-			photoContainer.innerHTML = `
-		
-		<div class="gallery-info">
-		<p>${video.user.name}</p>
-		<a  href = ${video_file.link} target = '_blank'> <i class="fas fa-download"></i> </a>
-		</div>
-		<video  src = ${video_file.link}></video>
-		`;
-			videosGallery.appendChild(photoContainer);
+			//=> Conditions:
+			if (
+				video_file.quality === 'hd' &&
+				video_file.width === 1280 &&
+				video_file.height === 720
+			) {
+				videoContainer.innerHTML = `
+				<div class="gallery-info">
+				<p>${video_file.quality}</p>
+				<a  href = ${video_file.link} target = '_blank'> <i class="fas fa-download"></i> </a>
+				</div>
+				<video muted src = ${video_file.link}></video>
+				`;
+			} else if (
+				(video_file.quality === 'sd' || video_file.width === 1920, 960, 640)
+			) {
+				videoContainer.style.display = 'none';
+			}
+
+			//=> Append to Videos Gallery:
+			videosGallery.appendChild(videoContainer);
+		});
+	});
+
+	//==> Preview and Play Videos:
+	const videos = videosGallery.querySelectorAll('video');
+
+	videos.forEach((video) => {
+		video.addEventListener('mouseover', function () {
+			this.play();
+		});
+
+		video.addEventListener('mouseout', function () {
+			this.pause();
+		});
+
+		video.addEventListener('touchstart', function () {
+			this.play();
+		});
+
+		video.addEventListener('touchend', function () {
+			this.pause();
 		});
 	});
 }
@@ -62,7 +118,7 @@ function generateVideos(data) {
 //==> Get Popular Videos:
 async function popularVideos() {
 	//=> Popular Videos url:
-	fetchVideosUrl = `https://api.pexels.com/videos/popular?per_page=16&page=1`;
+	fetchVideosUrl = `https://api.pexels.com/videos/popular?per_page=30&page=1`;
 
 	//=> Api fetch:
 	const data = await fetchApi(fetchVideosUrl);
@@ -71,9 +127,38 @@ async function popularVideos() {
 	generateVideos(data);
 }
 
+//==> Search Videos:
+async function searchVideos(query) {
+	//=> Clear Everythings:
+	clear();
+
+	//=> Search url:
+	fetchVideosUrl = `https://api.pexels.com/videos/search?query=${query}+query&per_page=25&page=1`;
+
+	//=> Api fetch:
+	const data = await fetchApi(fetchVideosUrl);
+
+	//=> Generate Videos:
+	generateVideos(data);
+}
+
 //==> LoadMore Videos:
-function loadMoreVideos() {
-	console.log('search succesful!');
+async function loadMoreVideos() {
+	//=> Page Increments:
+	page++;
+
+	//=> Conditions:
+	if (currentSearch) {
+		fetchVideosUrl = `https://api.pexels.com/videos/search?query=${currentSearch}+query&per_page=25&page=${page}`;
+	} else {
+		fetchVideosUrl = `https://api.pexels.com/videos/popular?per_page=30&page=${page}`;
+	}
+
+	//=> Api fetch:
+	const data = await fetchApi(fetchVideosUrl);
+
+	//=> Generate Videos:
+	generateVideos(data);
 }
 
 popularVideos();
