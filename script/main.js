1bb3d53be8b29e802872643e7bc26132d038d9a3
Saidul Islam Santo <sksanto.pro@gmail.com>
2022-12-15 15:27:23 +0600

    photo lightbox

--- script/main.js
+++ script/main.js
@@ -87,7 +87,36 @@ function generatePhotos(data) {
 		`;
 		photosGallery.appendChild(photoContainer);
 	});
+
+	imageLightbox();
 }
+
+//-->> gallery Image Lightbox:
+function imageLightbox() {
+	const lightbox = document.createElement('div');
+	lightbox.id = 'lightbox';
+	document.body.appendChild(lightbox);
+
+	const galleryImages = document.querySelectorAll('.photoContainer img');
+	galleryImages.forEach((image) => {
+		image.addEventListener('click', (e) => {
+			lightbox.classList.add('active');
+			const img = document.createElement('img');
+			img.src = image.src;
+			while (lightbox.firstChild) {
+				lightbox.removeChild(lightbox.firstChild);
+			}
+			lightbox.appendChild(img);
+		});
+	});
+
+	lightbox.addEventListener('click', (e) => {
+		if (e.target !== e.currentTarget) {
+			return lightbox.classList.remove('active');
+		}
+	});
+}
+imageLightbox();
 
 async function curatedPhotos() {
 	//=> curated url:
